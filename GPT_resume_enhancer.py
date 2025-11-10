cf5176e559372528bf9b240000d49000748c2ee6
Bhverma <happy.19rj@gmail.com>
2025-11-10 21:46:35 +0530

    Sec commit

--- GPT_resume_enhancer.py
+++ GPT_resume_enhancer.py
@@ -5,3 +5,4 @@ mistral_models_path = Path.home().joinpath('mistral_models', '7B-Instruct-v0.3')
 mistral_models_path.mkdir(parents=True, exist_ok=True)
 print(f"Downloading Mistral 7B-Instruct-v0.3 models to {mistral_models_path}...")
 # snapshot_download(repo_id="mistralai/Mistral-7B-Instruct-v0.3", allow_patterns=["params.json", "consolidated.safetensors", "tokenizer.model.v3"], local_dir=mistral_models_path)
+##################################################My name is Bharat"##########################################################
